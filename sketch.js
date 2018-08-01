d3bd972c8ba34793d0062c1a4dc141f214e43be5
Glitch (fcs-cs-make-a-circle2-mdarfler) <none>
2018-08-01 15:14:42 +0000

    ⛺️😳 Updated with Glitch

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let x,y = 0;
 function setup() {
   createCanvas(400, 400);
+  frameRate();
 }
 
 function draw() {
   background(220);
+  fill(255,25,71);
+  ellipse(100,y,50,50);
+  x++;
+  y++;
+  
+  if (x > width){
+    x = 0;
+  }
+  
+  if (y > height){
+    y = 0;
+  }
 }
